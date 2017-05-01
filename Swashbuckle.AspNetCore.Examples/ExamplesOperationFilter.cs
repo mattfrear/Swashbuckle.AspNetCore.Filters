376fe1fd4cddafd5b53c5bd84b4db650a34e8289
Matt Frear <matt.frear@gmail.com>
2017-05-01 20:14:07 +0100

    code compiles

--- Swashbuckle.AspNetCore.Examples/ExamplesOperationFilter.cs
+++ Swashbuckle.AspNetCore.Examples/ExamplesOperationFilter.cs
@@ -1,66 +1,75 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web.Http.Description;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using Swashbuckle.Swagger;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 
 namespace Swashbuckle.Examples
 {
     public class ExamplesOperationFilter : IOperationFilter
     {
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            SetRequestModelExamples(operation, context.SchemaRegistry, context.ApiDescription);
+            SetResponseModelExamples(operation, context.SchemaRegistry, context.ApiDescription);
+        }
+
         public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
         {
             SetRequestModelExamples(operation, schemaRegistry, apiDescription);
             SetResponseModelExamples(operation, schemaRegistry, apiDescription);
         }
 
-        private static void SetRequestModelExamples(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        private static void SetRequestModelExamples(Operation operation, ISchemaRegistry schemaRegistry, ApiDescription apiDescription)
         {
-            var requestAttributes = apiDescription.GetControllerAndActionAttributes<SwaggerRequestExamplesAttribute>();
+            var actionAttributes = apiDescription.ActionAttributes();
+            var swaggerRequestAttributes = actionAttributes.Where(r => r.GetType() == typeof(SwaggerRequestExamplesAttribute));
 
-            foreach (var attr in requestAttributes)
+            foreach (var attribute in swaggerRequestAttributes)
             {
+                var attr = (SwaggerRequestExamplesAttribute)attribute;
                 var schema = schemaRegistry.GetOrRegister(attr.RequestType);
 
-                var request = operation.parameters.FirstOrDefault(p => p.@in == "body" && p.schema.@ref == schema.@ref);
+                var request = operation.Parameters.FirstOrDefault(p => p.In == "body"/* && p.schema.@ref == schema.@ref */);
 
                 if (request != null)
                 {
                     var provider = (IExamplesProvider)Activator.CreateInstance(attr.ExamplesProviderType);
 
-                    var parts = schema.@ref.Split('/');
+                    var parts = schema.Ref.Split('/');
                     var name = parts.Last();
 
                     var definitionToUpdate = schemaRegistry.Definitions[name];
 
                     if (definitionToUpdate != null)
                     {
-                        definitionToUpdate.example = ((dynamic)FormatAsJson(provider))["application/json"];
+                        definitionToUpdate.Example = ((dynamic)FormatAsJson(provider))["application/json"];
                     }
                 }
             }
         }
 
-        private static void SetResponseModelExamples(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        private static void SetResponseModelExamples(Operation operation, ISchemaRegistry schemaRegistry, ApiDescription apiDescription)
         {
-            var responseAttributes = apiDescription.GetControllerAndActionAttributes<SwaggerResponseExamplesAttribute>();
+            var actionAttributes = apiDescription.ActionAttributes();
+            var swaggerRequestAttributes = actionAttributes.Where(r => r.GetType() == typeof(SwaggerRequestExamplesAttribute));
 
-            foreach (var attr in responseAttributes)
+            foreach (var attribute in swaggerRequestAttributes)
             {
+                var attr = (SwaggerResponseExamplesAttribute)attribute;
                 var schema = schemaRegistry.GetOrRegister(attr.ResponseType);
 
-                var response =
-                    operation.responses.FirstOrDefault(
-                        x => x.Value != null && x.Value.schema != null && x.Value.schema.@ref == schema.@ref);
+                var response = operation.Responses.FirstOrDefault(x => x.Value != null && x.Value.Schema != null && x.Value.Schema.Ref == schema.Ref);
 
                 if (response.Equals(default(KeyValuePair<string, Response>)) == false)
                 {
                     if (response.Value != null)
                     {
                         var provider = (IExamplesProvider)Activator.CreateInstance(attr.ExamplesProviderType);
-                        response.Value.examples = FormatAsJson(provider);
+                        response.Value.Examples = FormatAsJson(provider);
                     }
                 }
             }
