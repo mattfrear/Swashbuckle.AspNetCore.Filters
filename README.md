4d0efd28c22a4137de47b9ed7dd46e26c98f9bf8
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2017-06-13 15:12:38 +0300

    di for example providers

--- README.md
+++ README.md
@@ -4,3 +4,43 @@ Adds the [SwaggerRequestExample] and [SwaggerResponseExample] attributes to Swas
 Example request: https://mattfrear.com/2016/01/25/generating-swagger-example-requests-with-swashbuckle/
 
 Example response: https://mattfrear.com/2015/04/21/generating-swagger-example-responses-with-swashbuckle/
+
+## Examples with Dependency injection
+
+If for some reason you need to have examples with DI (for example read them from database) you may:
+
+Provide required dependencies in example provider classes:
+
+```
+internal class PersonRequestExample : IExamplesProvider
+{
+	private readonly IHostingEnvironment _env;
+
+	public PersonRequestExample(IHostingEnvironment env)
+	{
+		_env = env;
+	}
+	public object GetExamples()
+	{
+		return new PersonRequest { Age = 24, FirstName = _env.IsDevelopment() ? "Development" : "Production", Income = null };
+	}
+}
+```
+
+Register your example provider as a service in Startup:
+
+```
+services.AddTransient<PersonRequestExample>();
+```
+
+Pass service collection to ExamplesOperationFilter as a parameter:
+
+```
+services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+
+	c.OperationFilter<ExamplesOperationFilter>(services.BuildServiceProvider());
+	c.OperationFilter<DescriptionOperationFilter>();
+});
+```
--- src/Swashbuckle.AspNetCore.Examples/ExamplesOperationFilter.cs
+++ src/Swashbuckle.AspNetCore.Examples/ExamplesOperationFilter.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Newtonsoft.Json;
 using Swashbuckle.AspNetCore.Swagger;
@@ -8,95 +9,105 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Swashbuckle.AspNetCore.Examples
 {
-    public class ExamplesOperationFilter : IOperationFilter
-    {
-        public void Apply(Operation operation, OperationFilterContext context)
-        {
-            SetRequestModelExamples(operation, context.SchemaRegistry, context.ApiDescription);
-            SetResponseModelExamples(operation, context.ApiDescription);
-        }
+	public class ExamplesOperationFilter : IOperationFilter
+	{
+		private static IServiceProvider _services;
 
-        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
-        {
-            SetRequestModelExamples(operation, schemaRegistry, apiDescription);
-            SetResponseModelExamples(operation, apiDescription);
-        }
+		public ExamplesOperationFilter(IServiceProvider services = null)
+		{
+			_services = services;
+		}
 
-        private static void SetRequestModelExamples(Operation operation, ISchemaRegistry schemaRegistry, ApiDescription apiDescription)
-        {
-            var actionAttributes = apiDescription.ActionAttributes();
-            var swaggerRequestAttributes = actionAttributes.Where(r => r.GetType() == typeof(SwaggerRequestExampleAttribute));
+		public void Apply(Operation operation, OperationFilterContext context)
+		{
+			SetRequestModelExamples(operation, context.SchemaRegistry, context.ApiDescription);
+			SetResponseModelExamples(operation, context.ApiDescription);
+		}
 
-            foreach (var attribute in swaggerRequestAttributes)
-            {
-                var attr = (SwaggerRequestExampleAttribute)attribute;
-                var schema = schemaRegistry.GetOrRegister(attr.RequestType);
+		public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+		{
+			SetRequestModelExamples(operation, schemaRegistry, apiDescription);
+			SetResponseModelExamples(operation, apiDescription);
+		}
 
-                var request = operation.Parameters.FirstOrDefault(p => p.In == "body"/* && p.schema.@ref == schema.@ref */);
+		private static void SetRequestModelExamples(Operation operation, ISchemaRegistry schemaRegistry, ApiDescription apiDescription)
+		{
+			var actionAttributes = apiDescription.ActionAttributes();
+			var swaggerRequestAttributes = actionAttributes.Where(r => r.GetType() == typeof(SwaggerRequestExampleAttribute));
 
-                if (request != null)
-                {
-                    var provider = (IExamplesProvider)Activator.CreateInstance(attr.ExamplesProviderType);
+			foreach (var attribute in swaggerRequestAttributes)
+			{
+				var attr = (SwaggerRequestExampleAttribute)attribute;
+				var schema = schemaRegistry.GetOrRegister(attr.RequestType);
 
-                    var parts = schema.Ref.Split('/');
-                    var name = parts.Last();
+				var request = operation.Parameters.FirstOrDefault(p => p.In == "body"/* && p.schema.@ref == schema.@ref */);
 
-                    var definitionToUpdate = schemaRegistry.Definitions[name];
+				if (request != null)
+				{
+					var provider = _services == null
+						? (IExamplesProvider)Activator.CreateInstance(attr.ExamplesProviderType)
+						: (IExamplesProvider)_services.GetService(attr.ExamplesProviderType)
+						?? (IExamplesProvider)Activator.CreateInstance(attr.ExamplesProviderType);
 
-                    if (definitionToUpdate != null)
-                    {
-                        var serializerSettings = new JsonSerializerSettings
-                        {
-                            ContractResolver = attr.ContractResolver,
-                            NullValueHandling = NullValueHandling.Ignore // ignore null values because swagger does not support null objects https://github.com/OAI/OpenAPI-Specification/issues/229
-                        };
+					var parts = schema.Ref.Split('/');
+					var name = parts.Last();
 
-                        definitionToUpdate.Example = ((dynamic)FormatAsJson(provider, serializerSettings))["application/json"];
-                    }
-                }
-            }
-        }
+					var definitionToUpdate = schemaRegistry.Definitions[name];
 
-        private static void SetResponseModelExamples(Operation operation, ApiDescription apiDescription)
-        {
-            var actionAttributes = apiDescription.ActionAttributes();
-            var swaggerResponseExampleAttributes = actionAttributes.Where(r => r.GetType() == typeof(SwaggerResponseExampleAttribute));
+					if (definitionToUpdate != null)
+					{
+						var serializerSettings = new JsonSerializerSettings
+						{
+							ContractResolver = attr.ContractResolver,
+							NullValueHandling = NullValueHandling.Ignore // ignore null values because swagger does not support null objects https://github.com/OAI/OpenAPI-Specification/issues/229
+						};
 
-            foreach (var attribute in swaggerResponseExampleAttributes)
-            {
-                var attr = (SwaggerResponseExampleAttribute)attribute;
-                var statusCode = attr.StatusCode.ToString();
+						definitionToUpdate.Example = ((dynamic)FormatAsJson(provider, serializerSettings))["application/json"];
+					}
+				}
+			}
+		}
 
-                var response = operation.Responses.FirstOrDefault(r => r.Key == statusCode);
+		private static void SetResponseModelExamples(Operation operation, ApiDescription apiDescription)
+		{
+			var actionAttributes = apiDescription.ActionAttributes();
+			var swaggerResponseExampleAttributes = actionAttributes.Where(r => r.GetType() == typeof(SwaggerResponseExampleAttribute));
 
-                if (response.Equals(default(KeyValuePair<string, Response>)) == false)
-                {
-                    if (response.Value != null)
-                    {
-                        var provider = (IExamplesProvider)Activator.CreateInstance(attr.ExamplesProviderType);
-                        var serializerSettings = new JsonSerializerSettings { ContractResolver = attr.ContractResolver };
-                        response.Value.Examples = FormatAsJson(provider, serializerSettings);
-                    }
-                }
-            }
-        }
+			foreach (var attribute in swaggerResponseExampleAttributes)
+			{
+				var attr = (SwaggerResponseExampleAttribute)attribute;
+				var statusCode = attr.StatusCode.ToString();
 
-        private static object ConvertToDesiredCase(Dictionary<string, object> examples, JsonSerializerSettings serializerSettings)
-        {
-            var jsonString = JsonConvert.SerializeObject(examples, serializerSettings);
-            return JsonConvert.DeserializeObject(jsonString);
-        }
+				var response = operation.Responses.FirstOrDefault(r => r.Key == statusCode);
 
-        private static object FormatAsJson(IExamplesProvider provider, JsonSerializerSettings serializerSettings)
-        {
-            var examples = new Dictionary<string, object>
-            {
-                {
-                    "application/json", provider.GetExamples()
-                }
-            };
+				if (response.Equals(default(KeyValuePair<string, Response>)) == false)
+				{
+					if (response.Value != null)
+					{
+						var provider = (IExamplesProvider)Activator.CreateInstance(attr.ExamplesProviderType);
+						var serializerSettings = new JsonSerializerSettings { ContractResolver = attr.ContractResolver };
+						response.Value.Examples = FormatAsJson(provider, serializerSettings);
+					}
+				}
+			}
+		}
 
-            return ConvertToDesiredCase(examples, serializerSettings);
-        }
-    }
+		private static object ConvertToDesiredCase(Dictionary<string, object> examples, JsonSerializerSettings serializerSettings)
+		{
+			var jsonString = JsonConvert.SerializeObject(examples, serializerSettings);
+			return JsonConvert.DeserializeObject(jsonString);
+		}
+
+		private static object FormatAsJson(IExamplesProvider provider, JsonSerializerSettings serializerSettings)
+		{
+			var examples = new Dictionary<string, object>
+			{
+				{
+					"application/json", provider.GetExamples()
+				}
+			};
+
+			return ConvertToDesiredCase(examples, serializerSettings);
+		}
+	}
 }
