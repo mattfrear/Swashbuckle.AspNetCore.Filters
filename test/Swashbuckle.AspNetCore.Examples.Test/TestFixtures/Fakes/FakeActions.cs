3576c193e5c7c60ad1be30ba43b780144abc57f1
Matt Frear <matt.frear@gmail.com>
2018-01-26 11:14:20 +0000

    Add test infrastructure

---
+++ test/Swashbuckle.AspNetCore.Examples.Test/TestFixtures/Fakes/FakeActions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Swashbuckle.AspNetCore.SwaggerGen.Test
+{
+    public class FakeActions
+    {
+        [ProducesResponseType(typeof(void), 204)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
+        public IActionResult AnnotatedWithResponseTypeAttributes()
+        {
+            throw new NotImplementedException();
+        }
+
+        [SwaggerResponse(204, typeof(void), "No content is returned.")]
+        [SwaggerResponse(400, typeof(IDictionary<string, string>), "This returns a dictionary.")]
+        public IActionResult AnnotatedWithSwaggerResponseAttributes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
