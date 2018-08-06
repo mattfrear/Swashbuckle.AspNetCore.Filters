b7532e3cc5edbb26798149fe6d491b0516002df0
Matt Frear <Matt.Frear@runpath.com>
2018-08-06 12:02:55 +0100

    Made AppendAuthorizeToSummaryOperationFilter generic

---
+++ src/Swashbuckle.AspNetCore.Filters/AppendAuthorizeToSummary/AppendAuthorizeToSummaryOperationFilter.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Authorization;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+namespace Swashbuckle.AspNetCore.Filters
+{
+    public class AppendAuthorizeToSummaryOperationFilter : IOperationFilter
+    {
+        private readonly AppendAuthorizeToSummaryOperationFilter<AuthorizeAttribute> filter;
+
+        public AppendAuthorizeToSummaryOperationFilter()
+        {
+            var policySelector = new PolicySelectorWithLabel<AuthorizeAttribute>
+            {
+                Label = "policies",
+                Selector = authAttributes =>
+                    authAttributes
+                        .Where(a => !string.IsNullOrEmpty(a.Policy))
+                        .Select(a => a.Policy)
+            };
+
+            var rolesSelector = new PolicySelectorWithLabel<AuthorizeAttribute>
+            {
+                Label = "roles",
+                Selector = authAttributes =>
+                    authAttributes
+                        .Where(a => !string.IsNullOrEmpty(a.Roles))
+                        .Select(a => a.Roles)
+            };
+
+            filter = new AppendAuthorizeToSummaryOperationFilter<AuthorizeAttribute>(policySelector, rolesSelector);
+        }
+
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            filter.Apply(operation, context);
+        }
+    }
+}
---
+++ src/Swashbuckle.AspNetCore.Filters/AppendAuthorizeToSummary/AppendAuthorizeToSummaryOperationFilterT.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Authorization;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Swashbuckle.AspNetCore.Filters.Extensions;
+using System;
+
+namespace Swashbuckle.AspNetCore.Filters
+{
+    public class AppendAuthorizeToSummaryOperationFilter<T> : IOperationFilter where T : Attribute
+    {
+        private readonly Func<IEnumerable<T>, IEnumerable<string>> policySelector;
+        private readonly IEnumerable<PolicySelectorWithLabel<T>> policySelectors;
+
+        /// <summary>
+        /// Constructor for AppendAuthorizeToSummaryOperationFilter
+        /// </summary>
+        /// <param name="policySelectionCondition">Selects which attributes have policies. e.g. (a => !string.IsNullOrEmpty(a.Policy))</param>
+        /// <param name="policySelector">Used to select the authorization policy from the attribute e.g. (a => a.Policy)</param>
+        public AppendAuthorizeToSummaryOperationFilter(params PolicySelectorWithLabel<T>[] policySelectors)
+        {
+            this.policySelectors = policySelectors;
+        }
+
+        public void Apply(Operation operation, OperationFilterContext context)
+        {
+            if (context.GetControllerAndActionAttributes<AllowAnonymousAttribute>().Any())
+            {
+                return;
+            }
+
+            var authorizeAttributes = context.GetControllerAndActionAttributes<T>();
+
+            if (authorizeAttributes.Any())
+            {
+                var authorizationDescription = new StringBuilder(" (Auth");
+
+                foreach (var policySelector in policySelectors)
+                {
+                    AppendPolicies(authorizeAttributes, authorizationDescription, policySelector);
+                }
+
+                operation.Summary += authorizationDescription.ToString().TrimEnd(';') + ")";
+            }
+        }
+
+        private void AppendPolicies(IEnumerable<T> authorizeAttributes, StringBuilder authorizationDescription, PolicySelectorWithLabel<T> policySelector)
+        {
+            var policies = policySelector.Selector(authorizeAttributes)
+                .OrderBy(policy => policy);
+
+            if (policies.Any())
+            {
+                authorizationDescription.Append($" {policySelector.Label}: {string.Join(", ", policies)};");
+            }
+        }
+    }
+}
