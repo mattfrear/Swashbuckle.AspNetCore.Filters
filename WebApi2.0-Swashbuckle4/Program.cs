75cd1517b6828e5c1995a1e1a136f69f6cdd0619
Matt Frear <Matt.Frear@runpath.com>
2018-11-08 15:43:10 +0000

    Add Swashbuckle 4 test project

---
+++ WebApi2.0-Swashbuckle4/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApi2._0_Swashbuckle4
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
