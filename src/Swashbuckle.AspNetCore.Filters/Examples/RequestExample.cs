8ed49cbb32963c7791ca4e64c747c42cb4d1d3d2
Leon Volovyk <leon.volovyk@nexpay.com.au>
2018-11-12 15:16:51 +1100

    Ensure ReDoc doesn't throw warnings
    https://github.com/mattfrear/Swashbuckle.AspNetCore.Filters/issues/69

--- src/Swashbuckle.AspNetCore.Filters/Examples/RequestExample.cs
+++ src/Swashbuckle.AspNetCore.Filters/Examples/RequestExample.cs
@@ -1,4 +1,4 @@
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
@@ -47,7 +47,6 @@ namespace Swashbuckle.AspNetCore.Filters
             var serializerSettings = serializerSettingsDuplicator.SerializerSettings(contractResolver, jsonConverter);
 
             var formattedExample = jsonFormatter.FormatJson(example, serializerSettings, includeMediaType: false);
-            bodyParameter.Schema.Example = formattedExample; // set example on the paths/parameters/schema/example property
 
             string name = SchemaDefinitionName(requestType, schema);
 
@@ -64,6 +63,10 @@ namespace Swashbuckle.AspNetCore.Filters
                 {
                     definitionToUpdate.Example = formattedExample;
                 }
+            }
+            else
+            {
+                bodyParameter.Schema.Example = formattedExample; // set example on the paths/parameters/schema/example property
             }
         }
 
