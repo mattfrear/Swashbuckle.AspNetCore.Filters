c7ab008003c9525c45ed5a8fcb303711a5238cdf
Matt Frear <matt.frear@gmail.com>
2019-05-29 07:21:16 +0100

    Add .NET Core 3.0 preview 5 test project

---
+++ test/WebApi3.0-Swashbuckle5/Startup.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using WebApi.Models.Examples;
+
+namespace WebApi3._0_Swashbuckle5
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                .AddNewtonsoftJson();
+
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v2" });
+
+                options.ExampleFilters();
+
+                options.OperationFilter<AddHeaderOperationFilter>("correlationId", "Correlation Id for the request");
+
+                options.OperationFilter<AddResponseHeadersFilter>();
+
+                options.DescribeAllEnumsAsStrings();
+
+                var filePath = Path.Combine(AppContext.BaseDirectory, "WebApi.xml");
+                options.IncludeXmlComments(filePath);
+
+                options.OperationFilter<AppendAuthorizeToSummaryOperationFilter>();
+
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Description = "Standard Authorization header using the Bearer scheme. Example: \"bearer {token}\"",
+                    In = ParameterLocation.Header,
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
+
+            services.AddSwaggerExamplesFromAssemblyOf<PersonResponseExample>();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Administrator", authBuilder =>
+                {
+                    authBuilder.AddAuthenticationSchemes("bearer");
+                    authBuilder.RequireRole("Administrator");
+                });
+                options.AddPolicy("Customer", authBuilder =>
+                {
+                    authBuilder.AddAuthenticationSchemes("Bearer");
+                    authBuilder.RequireRole("Customer");
+                });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+        }
+    }
+}
