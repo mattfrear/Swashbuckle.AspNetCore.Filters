7df51ed195227eed9512349222d7ef3df82cde44
Brian Buvinghausen <buvinghausen@users.noreply.github.com>
2021-03-29 14:38:06 -0500

    Dependency cleanup
    Move Swagger UI & Annotations up to test projects where they are used

--- test/WebApi2.1-Swashbuckle5/Startup.cs
+++ test/WebApi2.1-Swashbuckle5/Startup.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
